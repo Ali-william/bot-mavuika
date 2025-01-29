dd4f0887bb7b721cf4013e0266bbff50324f1f88
TEBBICHE Ali <tebbicheali@MacBook-Air.local>
2025-01-29 23:10:28 +0100

    Ajout du projet

---
+++ config.js
@@ -0,0 +1,32 @@
+
+
+module.exports = {
+    TOKEN: "",
+    language: "en",
+    ownerID: ["1004206704994566164", ""], 
+    mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+    setupFilePath: './commands/setup.json',
+    commandsDir: './commands',  
+    embedColor: "#1db954",
+    activityName: "YouTube Music", 
+    activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+    SupportServer: "https://discord.gg/xQF9f9yUEM",
+    embedTimeout: 5, 
+    errorLog: "", 
+    nodes: [
+      {
+        name: "Catfein ID",
+        password: "catfein",
+        host: "lava.catfein.com",
+        port: 4000,
+        secure: false
+      },
+      { 
+        name: "saher",
+        password: "saher.inzeworld.com",
+        host: "lava.inzeworld.com",
+        port: 3128,
+        secure: false
+      }
+    ]
+  }
